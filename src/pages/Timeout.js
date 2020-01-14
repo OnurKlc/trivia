b9b163b9148729651c5f143fb67865c31282e87c
OnurKlc <onurkilic5@gmail.com>
2020-01-15 01:34:13 +0300

    timeout-page-icons

--- src/pages/Timeout.js
+++ src/pages/Timeout.js
@@ -1,11 +1,13 @@
 import React, {useContext} from "react";
 import styled from "styled-components";
+import {Link} from "react-router-dom";
 
 const Outer = styled.div`
+color: #4b4b4b;
   position: absolute;
   left: 50%;
   transform: translateX(-50%);
-  top: 30%;
+  top: 25%;
   .middleware {
   width: max-content;
   display: flex;
@@ -16,6 +18,26 @@ const Outer = styled.div`
   .icon {
   font-size: 160px;
   }
+  .warning {
+  margin-top: 30px;
+  font-size: 24px;
+  color: #f7f1e3;
+  }
+  .home-wrapper {
+  color: white;
+  a {
+  color: inherit;
+  text-decoration: none;
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  justify-content: center;
+  }
+  }
+  .home {
+  font-size: 60px;
+  margin-top: 30px;
+  }
 `
 
 const Timeout = () => {
@@ -23,8 +45,14 @@ const Timeout = () => {
   return (
     <Outer>
       <div className="middleware">
-      <i className="fa fa-hourglass-end icon"></i>
-      <div>Yeterli süre içinde cevap vermediniz:(</div>
+        <i className="fa fa-hourglass-end icon"></i>
+        <div className="warning">Yeterli süre içinde cevap veremediniz:(</div>
+        <div className="home-wrapper">
+          <Link to={"/"}>
+            <i className="fa fa-home home"></i>
+            <p>Başa Dön</p>
+          </Link>
+        </div>
       </div>
     </Outer>
   )
